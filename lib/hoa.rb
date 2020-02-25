7ee47eb35d193c05a55b968ccee266f79a546839
Will Brennan <willfbren@gmail.com>
2020-02-25 19:17:01 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,6 +8,5 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
-  add_character[show][name]
-  return show
+  BASE_HOA[:chipmunks] <<
 end
